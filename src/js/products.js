b7ff2d85b0e8ff01799759ec285d049886624614
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-07-28 15:16:49 -0600

    still don't have sort working, moving on to home page ajax for now

--- src/js/products.js
+++ src/js/products.js
@@ -6,21 +6,18 @@ $( document ).ready(function() {
       url: 'http://galvanize-student-apis.herokuapp.com/gcommerce/products'
     }).done(function(productInfo) {
       console.log(productInfo);
+
       productInfo.forEach(function(eachProduct) {
         $('.productList').append('<div class="fullProduct"><img class="productImage" src="images/productimg/' + eachProduct.id + '.jpeg"><p>' + eachProduct.description + '</p><br><p>' + eachProduct.price + '</p></div>')
+
       })
+
     }).fail(function(error) {
       console.log(error);
     });
   };
   getProductInfo();
 
-  $('#priceSort').on('click', function(event) {
-    // $('.fullProduct').sort(function(a, b){return a-b});
-  })
 
-  $('#sizeSort').on('click', function(event) {
-
-  })
 
 });
