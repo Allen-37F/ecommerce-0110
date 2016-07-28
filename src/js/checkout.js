4b569c483cf0d226206ccde3173d156091332071
Allen Fordham <allen.s.fordham@gmail.com>
2016-07-28 14:30:39 -0600

    Increased functionality of the checkout

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -2,14 +2,30 @@ $(document).ready(function(){
   console.log( "ready!" );
 
   $('#importShip').click(function(){
-    $('#firstNameBill').val($('#firstNameShip').val());
-    $('#lastNameBill').val($('#lastNameShip').val());
-    $('#address1Bill').val($('#address1Ship').val());
-    $('#address2Bill').val($('#address2Ship').val());
-    $('#cityBill').val($('#cityShip').val());
-    $('#zipBill').val($('#zipShip').val());
-    var state = $('#stateShip option:selected').val();
-    $('#stateBill option[value=' + state + ']').attr('selected','selected');
+    if($('#checkboxImport').is(':checked')){
 
+      $('#firstNameBill').val($('#firstNameShip').val());
+      $('#lastNameBill').val($('#lastNameShip').val());
+      $('#address1Bill').val($('#address1Ship').val());
+      $('#address2Bill').val($('#address2Ship').val());
+      $('#cityBill').val($('#cityShip').val());
+      $('#zipBill').val($('#zipShip').val());
+      var state = $('#stateShip option:selected').val();
+      $('#stateBill option[value=' + state + ']').attr('selected','selected');
+      validateBillShip();
+    } else {
+      $('#firstNameBill').val((''));
+      $('#lastNameBill').val((''));
+      $('#address1Bill').val((''));
+      $('#address2Bill').val((''));
+      $('#cityBill').val((''));
+      $('#zipBill').val((''));
+      validateBillShip();
+
+    }
   });
 });
+
+  function validateBillShip() {
+
+  }
