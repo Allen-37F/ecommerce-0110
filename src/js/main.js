180ced812eb21ec58079c04f965c4a1dc478814c
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-07-28 07:04:53 -0600

    tried building js for carousel from scratch, not ready to merge yet

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,55 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    var currentIndex = 0,
-    items = $('.carouselBox div'),
-    itemAmt = items.length;
+    var allPhotos = $('.carouselBox li');
 
-    function cycleItems() {
-      var item = $('.carouselBox div').eq(currentIndex);
-      items.hide();
-      item.css('display','inline-block');
+    console.log(allPhotos);
+
+    function cyclePhotos () {
+      for(li in allPhotos) {
+        $('li').css('display', 'inline-block');
+        $('li').fadeIn(200).delay(2000).fadeOut(200);
+        
+      }
     }
 
-    var autoSlide = setInterval(function() {
-      currentIndex += 1;
-      if (currentIndex > itemAmt - 1) {
-        currentIndex = 0;
-      }
-      cycleItems();
-    }, 3000);
 
-    $('.next').click(function() {
-      clearInterval(autoSlide);
-      currentIndex += 1;
-      if (currentIndex > itemAmt - 1) {
-        currentIndex = 0;
-      }
-      cycleItems();
-    });
+    //This code works, but won't reset the timer or cycle images on it's own once clicked. Gonna start fresh.
 
-    $('.prev').click(function() {
-      clearInterval(autoSlide);
-      currentIndex -= 1;
-      if (currentIndex < 0) {
-        currentIndex = itemAmt - 1;
-      }
-      cycleItems();
-    });
+    // var currentIndex = 0,
+    // items = $('.carouselBox li'),
+    // itemAmt = items.length;
+    //
+    // function cycleItems() {
+    //   var item = $('.carouselBox li').eq(currentIndex);
+    //   items.hide();
+    //   item.css('display','inline-block');
+    // }
+    //
+    // var autoSlide = setInterval(function() {
+    //   currentIndex += 1;
+    //   if (currentIndex > itemAmt - 1) {
+    //     currentIndex = 0;
+    //   }
+    //   cycleItems();
+    // }, 3000);
+    //
+    // $('.next').click(function() {
+    //   clearInterval(autoSlide);
+    //   currentIndex += 1;
+    //   if (currentIndex > itemAmt - 1) {
+    //     currentIndex = 0;
+    //   }
+    //   cycleItems();
+    // });
+    //
+    // $('.prev').click(function() {
+    //   clearInterval(autoSlide);
+    //   currentIndex -= 1;
+    //   if (currentIndex < 0) {
+    //     currentIndex = itemAmt - 1;
+    //   }
+    //   cycleItems();
+    // });
 
 });
