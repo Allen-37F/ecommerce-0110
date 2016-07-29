9bdc9fe707ac42b1e6fbf7ef68cb48f863e82bc8
Ryan Harings <ryanharings@hotmail.com>
2016-07-29 01:14:05 -0600

    added filter feature and reformatted code

--- src/js/products.js
+++ src/js/products.js
@@ -7,44 +7,75 @@ $( document ).ready(function() {
     $.ajax({
       url: 'http://galvanize-student-apis.herokuapp.com/gcommerce/products',
     }).done(function(productInfo) {
-      console.log(productInfo);
 
       productInfo.forEach(function(info) {
-        allProducts.push(new product(info.description, info.id, info.price, info.size))
+        allProducts.push(new Product(info.description, info.id, info.price, info.size))
       })
       console.log(allProducts);
-      console.log(typeof allProducts[1].price);
+
+      allProducts.forEach(function (item) {
+        return appendToDom(item);
+      })
     }).fail(function(error) {
       console.log(error);
     });
 
-    allProducts.forEach(appendToDom(this));
 
   };
   getProductInfo();
 
   $('#filterPrice').change(function() {
     var filterPrice= $('#filterPrice').val();
-    if (filterPrice === 'cheap') {
-      $()
+    if (filterPrice === 'product') {
+      $('.product').show();
+    } else if (filterPrice === 'cheap') {
+      $('.product').hide();
+      $('.cheap').show();
+    } else if (filterPrice === 'expensive') {
+      $('.product').hide();
+      $('.expensive').show();
     }
+  })
+
+  $('#filterSize').change(function() {
+    var filterSize= $('#filterSize').val();
+    if (filterSize === 'product') {
+      $('.product').show();
+    } else if (filterSize === 'Fingerboard') {
+      $('.product').hide();
+      $('.Fingerboard').show();
+    } else if (filterSize === 'Skateboard') {
+      $('.product').hide();
+      $('.Skateboard').show();
+    } else if (filterSize === 'Longboard') {
+      $('.product').hide();
+      $('.Longboard').show();
+    }
+    console.log(filterSize);
   })
 
 
 });
 
-function product(desc, id, price, size) {
+function Product(desc, id, price, size) {
   this.description = desc;
   this.id = id;
-  this.price = parseFloat(price.replace('$', ''));
+  this.price = parseFloat(price.replace('$', '')).toFixed(2);
+  this.range = priceRanges(price);
   this.size = sizeOfBoard(size);
   this.img = 'images/productimg/' + id + '.jpeg';
 }
 
 function appendToDom(eachProduct) {
-  $('.productList').append('<div class="product' + eachProduct.id + ' ' + eachProduct.size + '"><img class="productImage" src="' + eachProduct.img + '"><p class="description">' + eachProduct.description + '</p><br><p class="price">$' + eachProduct.price + '</p><br><p class="' + eachProduct.size + '">' + eachProduct.size + '</p></div>');
-  console.log('test');
-  console.log(allProducts);
+  $('.productList').append('<div class=" product product' + eachProduct.id + ' ' + eachProduct.size + ' ' + eachProduct.range + '"><img class="productImage" src="' + eachProduct.img + '"><p class="' + eachProduct.size + '">' + eachProduct.size + '</p><p class="description">' + eachProduct.description + '</p><p>$' + eachProduct.price + '</p></div>');
+}
+
+function priceRanges(num) {
+  var price = parseFloat(num.replace('$', '')).toFixed(2);
+  if (price <= 50) {
+    return 'cheap';
+  }
+  return 'expensive';
 }
 
 function sizeOfBoard(num) {
