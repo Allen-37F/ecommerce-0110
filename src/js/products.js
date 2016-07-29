7d3e15b14ebee749e04b9926d80f673c1fae933a
Ryan Harings <ryanharings@hotmail.com>
2016-07-28 22:25:55 -0600

    filter issues

--- src/js/products.js
+++ src/js/products.js
@@ -1,23 +1,58 @@
 $( document ).ready(function() {
   console.log( 'sanity check' );
 
+  var allProducts = [];
+
   function getProductInfo() {
     $.ajax({
-      url: 'http://galvanize-student-apis.herokuapp.com/gcommerce/products'
+      url: 'http://galvanize-student-apis.herokuapp.com/gcommerce/products',
     }).done(function(productInfo) {
       console.log(productInfo);
 
-      productInfo.forEach(function(eachProduct) {
-        $('.productList').append('<div class="fullProduct"><img class="productImage" src="images/productimg/' + eachProduct.id + '.jpeg"><p>' + eachProduct.description + '</p><br><p>' + eachProduct.price + '</p></div>')
-
+      productInfo.forEach(function(info) {
+        allProducts.push(new product(info.description, info.id, info.price, info.size))
       })
-
+      console.log(allProducts);
+      console.log(typeof allProducts[1].price);
     }).fail(function(error) {
       console.log(error);
     });
+
+    allProducts.forEach(appendToDom(this));
+
   };
   getProductInfo();
 
+  $('#filterPrice').change(function() {
+    var filterPrice= $('#filterPrice').val();
+    if (filterPrice === 'cheap') {
+      $()
+    }
+  })
 
 
 });
+
+function product(desc, id, price, size) {
+  this.description = desc;
+  this.id = id;
+  this.price = parseFloat(price.replace('$', ''));
+  this.size = sizeOfBoard(size);
+  this.img = 'images/productimg/' + id + '.jpeg';
+}
+
+function appendToDom(eachProduct) {
+  $('.productList').append('<div class="product' + eachProduct.id + ' ' + eachProduct.size + '"><img class="productImage" src="' + eachProduct.img + '"><p class="description">' + eachProduct.description + '</p><br><p class="price">$' + eachProduct.price + '</p><br><p class="' + eachProduct.size + '">' + eachProduct.size + '</p></div>');
+  console.log('test');
+  console.log(allProducts);
+}
+
+function sizeOfBoard(num) {
+  if (num < 2) {
+    return "Fingerboard";
+  } else if (num < 4) {
+    return "Skateboard";
+  } else {
+    return "Longboard";
+  }
+}
